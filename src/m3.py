4f61209eda34b88ea783da8ade2cc270b45b9ca9
mckayct <mckayct@rose-hulman.edu>
2019-03-18 10:06:09 -0400

    DONE

--- src/m3.py
+++ src/m3.py
@@ -123,9 +123,10 @@ def broken_1(m):
     #    **  For full credit you must appropriately
     #    **  use (call) the   is_prime   function that is DEFINED ABOVE.
     count = 0
-    for k in range(2 * m + 1):
-        if is_prime(m):
+    for k in range(2 * m + 2):
+        if is_prime(k) is True:
             count = count + 1
+
     return
 
 
